2de86cd5bcfb7d86acc743e374864dd9953da78b
jslusark <slusark.jessica@gmail.com>
2025-12-29 20:28:39 +0100

    add visual feedback when score changes

--- Assets/Scripts/LikesBarUI.cs
+++ Assets/Scripts/LikesBarUI.cs
@@ -7,15 +7,20 @@ public class LikesBarUI : MonoBehaviour
 
 
 
-    [SerializeField]
-    private RectTransform likesBar;
+    [SerializeField] private RectTransform likesBar;
+    [SerializeField] private SpriteRenderer heartIcon;      // SpriteRenderer component
+    [SerializeField] private Sprite heartBeat;              // normal heart sprite
+    [SerializeField] private Sprite heartBreak;             // broken heart sprite
+
+    public Color successColor = Color.red;
+    public Color failColor = Color.gray;
+
 
     public void SetStart(float levelScore, float levelTarget)
     {
         score = levelScore; // likes starting point
         target = levelTarget; // likes end point
         Debug.Log(" Score:" + score + " Target:" + target);
-        // UpdateLikesBar();
     }
 
     public void UpdateScore(float gain)
@@ -24,7 +29,24 @@ public class LikesBarUI : MonoBehaviour
         score = Mathf.Clamp(score, 0f, target);
         float newWidth = (score / target) * Width;
         likesBar.sizeDelta = new Vector2(newWidth, Height);
+        var barImage = likesBar.GetComponent<UnityEngine.UI.Image>();
+
+        if (gain < 0)
+        {
+            heartIcon.sprite = heartBreak;           // change sprite
+            // heartIcon.GetComponent<UnityEngine.UI.Image>().sprite = Resources.Load<UnityEngine.Sprite>("heart_break");
+            // likesBar.GetComponent<UnityEngine.UI.Image>().color = failColor;
+            barImage.color = failColor;
+        }
+        else
+        {
+            heartIcon.sprite = heartBeat;           // change sprite
+            // heartIcon.GetComponent<UnityEngine.UI.Image>().sprite = Resources.Load<UnityEngine.Sprite>("heart_beat");
+            // likesBar.GetComponent<UnityEngine.UI.Image>().color = successColor;
+            barImage.color = successColor;
+        }
         Debug.Log(gain + " Score:" + score);
-        // UpdateLikesBar();
+
+
     }
 }
