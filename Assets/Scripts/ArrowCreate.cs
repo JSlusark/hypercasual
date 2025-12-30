58225b6abcf053b74acf9658c9adcc90e45b2785
jslusark <slusark.jessica@gmail.com>
2025-12-30 14:44:54 +0100

    add first arrow highlight in move sequence

--- Assets/Scripts/ArrowCreate.cs
+++ Assets/Scripts/ArrowCreate.cs
@@ -4,13 +4,9 @@ using UnityEngine.InputSystem;
 
 public class ArrowCreate : MonoBehaviour
 {
-
-    // public float MoveSpeed = 2f;
-    // public float diffMax = 1f;
-    // public float diffMin = 0f;
-    // private bool isCorrect;
     private bool isMatching = false;
-    // public ArrowSlider arrowSlider;
+    public float Width = 1f;
+    public float Height = 1f;
 
 
     public enum Direction
@@ -20,6 +16,7 @@ public class ArrowCreate : MonoBehaviour
         downArrow,
         leftArrow
     }
+    public Direction direction;
 
     public static readonly Dictionary<Direction, float> ArrowType = new()
     {
@@ -29,8 +26,6 @@ public class ArrowCreate : MonoBehaviour
         { Direction.leftArrow, 90f }
     };
 
-    public Direction direction;
-    // private int ID;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -38,10 +33,6 @@ public class ArrowCreate : MonoBehaviour
     {
         direction = (Direction)Random.Range(0, 4);
         transform.rotation = Quaternion.Euler(0f, 0f, ArrowType[direction]);
-        // ID = GetInstanceID();
-        // arrowList.AddArrow(gameObject);
-        // Debug.Log("COMP First arrow ID: " + arrowList.firstArrowID);
-        // Debug.Log("COMP First arrow ID: " + ID);
 
     }
 
--- Assets/Scripts/ArrowSlider.cs
+++ Assets/Scripts/ArrowSlider.cs
@@ -4,32 +4,42 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine.InputSystem;
 
+/*
+
+consecutive commands might not be best,
+fruit ninja for example has calm periods between series of swipes
+and would be better to implement that later in anothe branch to
+see if it works better for gameplay.
+
+smaller levels have smaller swipes (3 arrows)
+bigger levels have longer swipes (max 10 arrows then complicates with special arrows)
+
+ */
 
 public class ArrowSlider : MonoBehaviour
 {
-    private List<GameObject> arrowList = new();
-    public GameObject arrow;
-    private ArrowCreate firstArrow;
-    private int arrowCount = 5;
-    // starting position of list
-    private Vector3 listPosition = new Vector3(1f, 3f, 0f);
-    private float spacing = 1.5f;
-
+    // imported components that will interact with this script
     public LikesBarUI likesBar;
+    public GameObject arrow;
+
+    // list item
+    private List<GameObject> arrowSequence = new();
+    private ArrowCreate firstArrow;
+    public int arrowCount = 5;
+    // starting position of list
+    public Vector3 listPosition = new Vector3(-0.5f, 3f, 0f);
+    public float spacing = 1.2f;
 
 
-
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         for (int i = 0; i < arrowCount; i++)
         {
             GameObject newArrow = Instantiate(arrow);
             newArrow.transform.position = listPosition + new Vector3(i * spacing, 0f, 0f);
-            AddArrow(newArrow);
+            arrowSequence.Add(newArrow);
         }
-        firstArrow = arrowList[0].GetComponent<ArrowCreate>();
+        getFirstInSequence();
     }
 
     // Update is called once per frame
@@ -38,36 +48,34 @@ public class ArrowSlider : MonoBehaviour
 
     }
 
-    public void AddArrow(GameObject arrow)
+
+    public void getFirstInSequence()
     {
-        arrowList.Add(arrow);
+        if (arrowSequence.Count > 0)
+        {
+            firstArrow = arrowSequence[0].GetComponent<ArrowCreate>();
+            firstArrow.GetComponent<SpriteRenderer>().color = Color.yellow;
+        }
     }
 
     public void RemoveArrow()
     {
-        if (arrowList.Count == 0)
+        if (arrowSequence.Count == 0)
             return;
-
-        GameObject removed = arrowList[0];
-        arrowList.RemoveAt(0);
-        Destroy(removed);
-
-        if (arrowList.Count > 0)
-            firstArrow = arrowList[0].GetComponent<ArrowCreate>();
-
-        GameObject newArrow = Instantiate(arrow);
-        AddArrow(newArrow);
-        RepositionArrows();
-
-
+        GameObject removedArrow = arrowSequence[0];
+        arrowSequence.RemoveAt(0);
+        Destroy(removedArrow);
     }
 
 
-    private void RepositionArrows()
+    private void ContinueArrowSequence()
     {
-        for (int i = 0; i < arrowList.Count; i++)
+        getFirstInSequence(); // highlight new first arrow
+        GameObject newArrow = Instantiate(arrow);
+        arrowSequence.Add(newArrow);
+        for (int i = 0; i < arrowSequence.Count; i++)
         {
-            arrowList[i].transform.position =
+            arrowSequence[i].transform.position =
                 listPosition + new Vector3(i * spacing, 0f, 0f);
         }
     }
@@ -77,19 +85,22 @@ public class ArrowSlider : MonoBehaviour
         if (context.performed)
         {
             Debug.Log("DanceMove pressed: " + context.control.name);
-            // firstArrow = arrowList[0].GetComponent<ArrowCreate>();
 
             if (context.control.name == firstArrow.direction.ToString())
             {
-                likesBar.UpdateScore(+1f); // - 1 or multiplier if combo chain active
-                Debug.Log("💔 -1  | Score:" + likesBar.score);
+                likesBar.UpdateScore(+20f); // - 1 or multiplier if combo chain active
+                Debug.Log("💔 +100  | Score:" + likesBar.score);
                 Debug.Log("Matched arrow: " + firstArrow.direction + "point +1");
                 RemoveArrow();
+                // if max likes is not reached
+                ContinueArrowSequence();
+                // else level complete (add 1 video, show dancanimation and create a new sequence from scratch)
+
             }
             else
             {
-                likesBar.UpdateScore(-1f); // - 1 or multiplier if combo chain active
-                Debug.Log("💔 -1  | Score:" + likesBar.score);
+                likesBar.UpdateScore(-10f); // - 1 or multiplier if combo chain active
+                Debug.Log("💔 -50  | Score:" + likesBar.score);
                 Debug.Log("Mismatched arrow. Expected: " + firstArrow.direction + "point -1");
             }
         }
